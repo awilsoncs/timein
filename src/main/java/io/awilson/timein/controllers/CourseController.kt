56d890b640cb409087708f8ce84770e011a3f448
Aaron Wilson <acwilsoncs@gmail.com>
2017-07-05 19:58:18 -0400

    Add more API endpoints for Courses

--- src/main/java/io/awilson/timein/controllers/CourseController.kt
+++ src/main/java/io/awilson/timein/controllers/CourseController.kt
@@ -5,50 +5,49 @@ import io.awilson.timein.services.CourseService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.*
 
 /**
  * Provide a Controller for the Course class.
  */
 @Controller
+@RequestMapping("/courses")
 class CourseController {
 
     @Autowired
     lateinit var courseService: CourseService
 
-    @RequestMapping("course/new")
-    fun newCourse(model: Model): String {
+    @RequestMapping("/new")
+    fun new(model: Model): String {
         model.addAttribute("course", Course())
         return "courseform"
     }
 
-    @RequestMapping(value = "course", method = arrayOf(RequestMethod.POST))
-    fun saveCourse(course: Course): String {
+    @PostMapping
+    fun post(course: Course): String {
         courseService.saveCourse(course)
         return "redirect:/course/" + course.id
     }
 
-    @RequestMapping("course/{id}")
-    fun showCourse(@PathVariable id: Int, model: Model): String {
+    @RequestMapping("/{id}")
+    fun show(@PathVariable id: Int, model: Model): String {
         model.addAttribute("course", courseService.getCourseById(id))
         return "courseshow"
     }
 
-    @RequestMapping(value = "/courses", method = arrayOf(RequestMethod.GET))
+    @GetMapping
     fun list(model: Model): String {
         model.addAttribute("courses", courseService.listAllCourses())
         return "courses"
     }
 
-    @RequestMapping("course/edit/{id}")
+    @RequestMapping("/{id}/edit")
     fun edit(@PathVariable id: Int, model: Model): String {
         model.addAttribute("course", courseService.getCourseById(id))
         return "courseform"
     }
 
-    @RequestMapping("course/delete/{id}")
+    @RequestMapping("/{id}/delete")
     fun delete(@PathVariable id: Int): String {
         courseService.deleteCourse(id)
         return "redirect:/courses"
--- src/main/java/io/awilson/timein/controllers/api/CourseApiController.kt
+++ src/main/java/io/awilson/timein/controllers/api/CourseApiController.kt
@@ -2,29 +2,47 @@ package io.awilson.timein.controllers.api
 
 import io.awilson.timein.domain.Course
 import io.awilson.timein.services.CourseService
-import lombok.extern.log4j.Log4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+
 
 @RestController
 @RequestMapping("/api/courses")
 class CourseApiController {
     @Autowired lateinit var service: CourseService
 
-    @GetMapping
-    fun get(@RequestParam(value="id", defaultValue = "0") id: Int): ResponseEntity<Course> {
+    /**
+     * Provide a get mapping endpoint for single Courses
+     */
+    @GetMapping("/{id}")
+    fun get(@PathVariable id: Int): ResponseEntity<Course> {
         val course : Course = service.getCourseById(id) ?: throw Exception("Could not find Course with id:$id")
         return ResponseEntity<Course>(course, HttpStatus.OK)
     }
 
+    /**
+     * Provide a GET mapping endpoint for all Courses
+     */
+    @GetMapping
+    fun get(): ResponseEntity<Iterable<Course>> {
+        val courses: Iterable<Course> = service.listAllCourses()
+        return ResponseEntity<Iterable<Course>>(courses, HttpStatus.OK)
+    }
+
+    /**
+     * Provide a POST endpoint for Courses
+     */
     @PostMapping
     fun post(@RequestBody course: Course): ResponseEntity<Course> {
         val updated = service.saveCourse(course)
         return ResponseEntity<Course>(updated, HttpStatus.OK)
     }
 
+    /**
+     * Provide a DELETE endpoint for Courses
+     */
     @DeleteMapping
     fun delete(@RequestParam(value="id", defaultValue = "0") id: Int): HttpStatus {
         service.deleteCourse(id)
